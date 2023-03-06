26f219eba22fcb75829040645567ee80b89edaf5
LangPham <phamxuanlang@gmail.com>
2023-03-06 17:45:30 +0700

    Add yasix

--- index.js
+++ index.js
@@ -1,31 +1,64 @@
 import * as d3 from "d3";
+import Swal from "sweetalert2";
+import moment from "moment";
 
-// axis
-// .ticks(2)
-// .render()
-// Sample dataset. In a real application, you will probably get this data from another source such as AJAX.
-console.log(Date.now());
+// Dataset
 var dataset = [
-  { start: Date.now(), end: Date.now() + 60 * 60 * 1000, id: 1 },
-  // { start: 70, end: 90, id: 2 },
+  // { start: new Date, end: Date.now() + 60 * 60 * 1000, id: 1 },
+  {
+    start: new Date(2023, 2, 6, 19, 0, 0),
+    end: new Date(2023, 2, 6, 19, 45, 0),
+    id: 1,
+    content: "Dragon",
+    group: "one"
+  },
+  //
+  {
+    start: new Date(2023, 2, 6, 20, 0, 0),
+    end: new Date(2023, 2, 6, 21, 0, 0),
+    id: 2,
+    content: "Lion",
+    group: "two"
+  }, { start: 70, end: 90, id: 2 },
 ];
 
-var padding = 10;
-var margin = { top: 20, right: 0, bottom: 30, left: 0 };
-var height = 500;
-var width = 1000;
-var xtime = d3
+// var padding = 10;
+var margin = { top: 20, right: 20, bottom: 30, left: 40 };
+var height = 400;
+var width = 1200;
+var xTime = d3
   .scaleTime()
   .domain([
     Date.now() - 24 * 60 * 60 * 1000 * 5,
     Date.now() + 24 * 60 * 60 * 1000 * 45,
   ])
   .nice(2)
-  .range([padding, width - padding]);
+  .range([margin.left, width - margin.right]);
+
+var yGroup = d3
+  .scaleBand()
+  .domain(["one", "two", "three", "four"])
+  .range([height - margin.bottom, margin.top])
+  .padding(0.3);
+
+var svg = d3
+  .select("#mychart") // I'm starting off by selecting the container.
+  .append("svg") // Appending an SVG element to that container.
+  .attr("viewBox", [0, 0, width, height])
+  .attr("width", width) // Setting the width of the SVG.
+  .attr("height", height)
+
+var xAxis = (g, x) =>
+  g
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x));
+
+var gx = svg.append("g").call(xAxis, xTime);
+// const tooltip = svg.append("g").style("pointer-events", "none");
+svg.append("g").attr("transform", `translate(${margin.left},0)`).call(d3.axisLeft(yGroup));
 
 var zoom = d3
   .zoom()
-  // .scaleExtent([1, 50])
   .extent([
     [margin.left, 0],
     [width - margin.right, height],
@@ -36,13 +69,6 @@ var zoom = d3
   ])
   .on("zoom", zoomed);
 
-var svg = d3
-  .select("#mychart") // I'm starting off by selecting the container.
-  .append("svg") // Appending an SVG element to that container.
-  .attr("viewBox", [0, 0, width, height])
-  .attr("width", width) // Setting the width of the SVG.
-  .attr("height", height);
-
 svg
   .append("g")
   .attr("class", "rect")
@@ -52,122 +78,145 @@ svg
   .enter()
   .append("rect") // Then I'm mapping the dataset to those rectangles.
   .attr("x", function (value, index) {
-    return xtime(value.start);
+    return xTime(value.start);
   })
   .attr("y", function (value, index) {
-    return 0;
+    console.log("value", value.group)
+    console.log(yGroup.bandwidth("two"))
+    console.log("aAAAAAAA", yGroup(value.group))
+    return yGroup(value.group);
   })
   .attr("width", function (value, index) {
-    return xtime(value.end) - xtime(value.start);
+    return xTime(value.end) - xTime(value.start);
   }) //chartWidth / dataset.length - padding) // The width is dynamically calculated to have an even distribution of bars that take up the entire width of the chart.
-  .attr("height", function (value, index) {
-    // The height is simply the value of the item in the dataset multiplied by the height scaling factor.
-    // return value * heightScalingFactor;
-    return 20;
-  })
+  .attr("height", yGroup.bandwidth())
   .attr("fill", "pink") // Sets the color of the bars.
   .attr("data-id", "chi test")
   .on("click", function (d, i) {
-    console.log(d);
-    console.log(i);
+    console.log("event", d);
+    console.log("data", i);
+    title = "Edit book";
+    text = "Edit book with what";
+
+    Swal.fire({
+      title: "Action",
+      input: "radio",
+      inputOptions: {
+        edit: "Edit",
+        remove: "Remove",
+      },
+      inputValidator: (value) => {
+        if (!value) {
+          return "You need to choose something!";
+        }
+      },
+      showCancelButton: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        if (result.value == "edit") {
+          inputValue = moment(i.end).diff(moment(i.start), "minutes"); // 1 moment(i.end) - moment(i.start)
+          console.log("inputValue", inputValue);
+          Swal.fire({
+            title: "Time long you book?",
+            icon: "question",
+            input: "range",
+            inputLabel: "Minute",
+            inputAttributes: {
+              min: 60,
+              max: 180,
+              step: 15,
+            },
+            inputValue: inputValue,
+          }).then((result) => {
+            console.log("result", result);
+            console.log("before", moment(i.start));
+            i.end = moment(i.start)
+              .add(parseInt(result.value), "minutes")
+              .toDate();
+            console.log("after", i);
+            // console.log(d.target);
+            var transform = d3.zoomTransform(d.target);
+            console.log("transform", transform.rescaleX(xTime));
+            let xz = transform.rescaleX(xTime);
+            console.log("start", xz(i.start));
+            console.log("end", xz(i.end));
+            console.log("width", xz(i.end) - xz(i.start));
+            d3.select(d.target).attr("width", xz(i.end) - xz(i.start));
+
+            // d.target.attr("width", 500)
+          });
+        } else if (result.value == "remove") {
+          i = null;
+          d3.select(d.target).remove();
+        }
+      }
+    });
+  })
+  // Tooltip
+  .on("pointerenter", (event, d) => {
+    console.log(event, d);
+    tooltip = svg.append("g").attr("id", `tooltip-${d.id}`);
+    title = "ddddd";
+    tooltip.style("display", null);
+    tooltip.attr("transform", `translate(${event.clientX},${event.clientY})`);
+
+    const path = tooltip
+      .selectAll("path")
+      .data([,])
+      .join("path")
+      .attr("fill", "#f0f0f0")
+      .attr("stroke", "black");
+
+    const text = tooltip
+      .selectAll("text")
+      .data([,])
+      .join("text")
+      .call((text) =>
+        text
+          .selectAll("tspan")
+          .data(
+            `${d.content}: ${moment(d.start).format(
+              "DD/MM/YYYY HH:mm"
+            )} -> ${moment(d.end).format("HH:mm")} `
+          )
+          .join("tspan")
+          .attr("font-weight", (_, i) => (i ? null : "bold"))
+          .text((d) => d)
+      );
+
+    const { x, y, width: w, height: h } = text.node().getBBox();
+    text.attr("transform", `translate(${-w / 2},${15 - y})`);
+    path.attr(
+      "d",
+      `M${-w / 2 - 10},5H-5l5,-5l5,5H${w / 2 + 10}v${h + 20}h-${w + 20}z`
+    );
+    // svg.property("value", O[i]).dispatch("input", { bubbles: true });
+  })
+  .on("pointerleave", (event, d) => {
+    tooltip.remove();
+    svg.node().value = null;
+    svg.dispatch("input", { bubbles: true });
   });
-
-var xAxis = (g, x) =>
-  g
-    .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(x));
-
-var gx = svg.append("g").call(xAxis, xtime);
 
 svg
   .call(zoom)
   .transition()
   .duration(150)
-  .call(zoom.scaleTo, 50, [xtime(Date.now()), 0]);
+  .call(zoom.scaleTo, 50, [xTime(Date.now()), 0]);
 
 function zoomed(event) {
-  let xz = event.transform.rescaleX(xtime);
-  console.log("xz", event.transform);
+  let xz = event.transform.rescaleX(xTime);
+  // console.log("xz", event.transform);
   // path.attr("d", area(data, xz));
   svg
     .selectAll(".rect rect")
     .attr("x", function (d) {
-      //   console.log("d", d);
-      xtime(d.start);
-      //   console.log("d", xtime(d.start));
       return xz(d.start);
     })
     .attr("width", function (d) {
-      console.log("gia tri width", xz(d.end), xz(d.start));
-      // xz()
+      // console.log("gia tri width", xz(d.end), xz(d.start));
       return xz(d.end) - xz(d.start);
     });
-  //   console.log(svg.selectAll(".rect rect"))
+
   gx.call(xAxis, xz);
 }
-
-//   .attr("width", width - margin.left - margin.right) // Setting the width of the SVG.
-//   .attr("height", height - margin.top - margin.bottom) // And setting the height of the SVG.
-//   .attr("background", "red")
-//   .call(zoom);
-
-// var g = svg
-//   .append("g")
-//   .attr("transform", "translate(0,0) scale(1)")
-//   .call(xAxis, xtime);
-//   .call(zoom);
-// .call(g => g.select(".domain")
-// .remove())
-console.log(svg);
-
-// The next step is to create the rectangles that will make up the bars in our bar chart.
-// svg
-//   .selectAll("rect") // I'm selecting all of the rectangles in the SVG (note that at this point, there actually aren't any, but we'll be creating them in a couple of steps).
-//   .data(dataset) // Then I'm mapping the dataset to those rectangles.
-//   .enter() // This step is important in that it allows us to dynamically create the rectangle elements that we selected previously.
-//   .append("rect") // For each element in the dataset, append a new rectangle.
-//   .attr("x", function (value, index) {
-//     // Set the X position of the rectangle by taking the index of the current item we are creating, multiplying it by the calculated width of each bar, and adding a padding value so we can see some space between bars.
-//     return index * (chartWidth / dataset.length) + padding;
-//   })
-//   .attr("y", function (value, index) {
-//     // Set the rectangle by subtracting the scaled height from the height of the chart (this has to be done becuase SVG coordinates start with 0,0 at their top left corner).
-//     return chartHeight - value * heightScalingFactor;
-//   })
-//   .attr("width", chartWidth / dataset.length - padding) // The width is dynamically calculated to have an even distribution of bars that take up the entire width of the chart.
-//   .attr("height", function (value, index) {
-//     // The height is simply the value of the item in the dataset multiplied by the height scaling factor.
-//     return value * heightScalingFactor;
-//   })
-//   .attr("fill", "pink") // Sets the color of the bars.
-//   .attr("data-id", "chi test")
-//   .on("click", function (d, i) {
-//     console.log(d);
-//     console.log(i);
-//   });
-/**
- *  Gets the maximum value in a collection of numbers.
- */
-// function getMax(collection) {
-//   var max = 0;
-
-//   collection.forEach(function (element) {
-//     max = element > max ? element : max;
-//   });
-
-//   return max;
-// }
-
-// function spanOver(d, i) {
-//   var span = d3.select(this);
-//   span.classed("spanOver", true);
-// }
-
-// function spanOut(d, i) {
-//   var span = d3.select(this);
-//   span.classed("spanOver", false);
-// }
-
-// var div = d3.select('#divID');
-
-// svg.selectAll('span')
